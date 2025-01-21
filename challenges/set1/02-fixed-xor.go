afbf86aee2ddf1e4a6b428d6c0d2c31df5825552
sodesu2077 <nino.mirziashvili7@gmail.com>
2025-01-21 17:19:03 +0400

    set1.02 fixed xor

--- challenges/set1/02-fixed-xor.go
+++ challenges/set1/02-fixed-xor.go
@@ -1,3 +1,30 @@
 package set1
 
-func FixedXOR() {}
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/sodesu2077/matasano-crypto-go/utils"
+)
+
+func FixedXOR(buf1 string, buf2 string) (string, error) {
+	buf1Data, err := DecodeHexString(buf1)
+
+	utils.HandleErr(err)
+
+	buf2Data, err := DecodeHexString(buf2)
+
+	utils.HandleErr(err)
+
+	result := ""
+
+	for i := 0; i < len(buf1Data); i += 1 {
+		xord := buf1Data[i] ^ buf2Data[i]
+		value := strconv.FormatInt(int64(xord), 16)
+		result += value
+	}
+
+	fmt.Println(result)
+
+	return result, nil
+}
