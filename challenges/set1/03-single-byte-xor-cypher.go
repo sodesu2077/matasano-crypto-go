f982f2b13a8943005b3d704fd44557c3ea34edb3
sodesu2077 <nino.mirziashvili7@gmail.com>
2025-01-31 22:02:04 +0400

    set1.03 single byte xor cypher

--- challenges/set1/03-single-byte-xor-cypher.go
+++ challenges/set1/03-single-byte-xor-cypher.go
@@ -2,6 +2,7 @@ package set1
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/sodesu2077/matasano-crypto-go/utils"
@@ -16,26 +17,38 @@ func SingleByteXORCypher(hex string) ([]byte, byte, error) {
 	hexToBytes, err := DecodeHexString(hex)
 	utils.HandleErr(err)
 
-	usage := make(map[byte]byte)
-	topFrequentBytes := make(map[byte]byte)
+	data, key, err := FindSingleByteXOR(hexToBytes)
+	fmt.Println("string before XOR: ", string(data), "\bXOR key: ", string(key))
+	return data, key, err
+}
 
-	for _, b := range hexToBytes {
+func FindSingleByteXOR(data []byte) ([]byte, byte, error) {
+	usage := make(map[byte]int)
+	topFrequentBytes := make(map[byte]int)
+
+	for _, b := range data {
 		usage[b] += 1
 		if usage[b] > 2 {
 			topFrequentBytes[b] = usage[b]
 		}
 	}
 
-	for b, _ := range topFrequentBytes {
+	sortedTopFr := make([]byte, len(topFrequentBytes))
+	for k := range topFrequentBytes {
+		sortedTopFr = append(sortedTopFr, k)
+	}
+	sort.SliceStable(sortedTopFr, func(a, b int) bool {
+		return topFrequentBytes[sortedTopFr[a]] > topFrequentBytes[sortedTopFr[b]]
+	})
 
+	for _, b := range sortedTopFr {
 		for _, k := range FrequentChars {
 			key := b ^ byte(k[0])
-			var result []byte
-			for _, b := range hexToBytes {
-				result = append(result, byte(key)^b)
+			result := make([]byte, len(data))
+			for i, b := range data {
+				result[i] = byte(key) ^ b
 			}
-			if !strings.ContainsAny(string(result), "`@#$%^&*()_+=></~[|{}]") {
-				fmt.Println("string before XOR: ", string(result), "\nXOR key: ", string(key))
+			if len(result) != 0 && !strings.ContainsAny(string(result), "`@#$%^&*()_+=></~[|{}]��") {
 				return result, key, nil
 			}
 		}
