e60cd31ec16b3faf54581fa01c9bf2a8f4aa226b
sodesu2077 <nino.mirziashvili7@gmail.com>
2025-02-10 09:23:14 +0400

    set1 update project layout

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/sodesu2077/matasano-crypto-go/challenges/set1"
+	"github.com/sodesu2077/matasano-crypto-go/internal/set1"
 )
 
 func main() {
-	// fmt.Println(string(set1.DecryptAES("./static/data/set1.07.txt", []byte("YELLOW SUBMARINE"))))
 	fmt.Println(string(set1.DetectAES("./static/data/set1.08.txt")))
 }
